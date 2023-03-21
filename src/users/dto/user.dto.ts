bfea0f060188003f8ca63c9fc60c1d50f48b2228
Cristina Garcia <cgarciasan@hiberus.com>
2023-03-21 13:41:38 +0100

    created validation pipes for dtos

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UserDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   username: string;
-
-  @ApiProperty()
-  password: string;
 }
